36d06c4fad84325dc7030d1e49101aff235221df
Rohan Ramasamy <Rohan@Rohans-MacBook-Pro.local>
2017-08-09 22:48:57 +0100

    #22 - Added reference to C. K. Birdsall and new directory for es1 python implementation

---
+++ pic/es1/__init__.py
@@ -0,0 +1,5 @@
+"""
+This directory contains a re-implementation of the code ES1, as originally outlined in:
+
+Plasma Physics via Computer Simulation - C. K. Birdsall
+"""
---
+++ pic/es1/controller.py
@@ -0,0 +1,3 @@
+"""
+This file contains the main controller of ES1, used to control the main time step of simulations
+"""

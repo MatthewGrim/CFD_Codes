c351be35b474d705a7920818c9148264f6fb148e
Rohan Ramasamy <Rohan@rohans-mbp.home>
2017-08-24 17:15:59 +0100

    #22 - Added particle initialisers tests to files and a perturbed distribution so langmuir oscillations can be simulated. When this is done, ES1 is effectively complete

--- pic/es1/field_solver/field_solvers.py
+++ pic/es1/field_solver/field_solvers.py
@@ -22,14 +22,19 @@ class FieldSolvers(object):
         :param domain_length: total domain size
         :return:
         """
-        assert np.all(particle_positions < domain_length) and np.all(particle_positions > domain_length)
+        assert np.all(particle_positions < domain_length) and np.all(particle_positions > 0.0)
 
         for x in particle_positions:
-            i = x // dx
-            grid_densities[i] += (x - (i * dx)) / dx * Q
-            grid_densities[i + 1] += (x - (i * dx)) / dx * Q
-
-        return grid_densities
+            if x < 0.5 * dx:
+                grid_densities[0] += Q
+            elif x > domain_length - 0.5 * dx:
+                grid_densities[-1] += Q
+            else:
+                i = (x - 0.5 * dx) // dx
+                left_ratio = (x - (i + 0.5) * dx) / dx
+                right_ratio = 1.0 - left_ratio
+                grid_densities[i] += left_ratio * Q
+                grid_densities[i + 1] += right_ratio * Q
 
 
     @staticmethod
@@ -90,7 +95,44 @@ class FieldSolvers(object):
                 E_field[i] = -(potential_field[i + 1] - potential_field[i - 1]) / (2 * dx)
 
 
-if __name__ == '__main__':
+# --- TESTS ---
+def get_grid_densities_test():
+    """
+    Simple sinusoidal particle distribution to see if the expected density field is recovered
+    """
+    num_cells = 200
+    domain_length = 2 * np.pi
+    dx = domain_length / num_cells
+
+    rho = np.zeros(num_cells)
+    x = np.linspace(dx, domain_length - dx, num_cells)
+
+    n_0 = 100
+    n = n_0 * np.abs(np.sin(x))
+    particle_positions = list()
+    for i, x_loc in enumerate(x):
+        number_of_particles_in_cell = int(n[i])
+        particle_dx = dx / number_of_particles_in_cell
+        for j in range(number_of_particles_in_cell):
+            new_loc = x_loc - dx / 2 + j * particle_dx
+
+            particle_positions.append(new_loc)
+
+    particle_positions = np.asarray(particle_positions)
+
+    FieldSolvers.get_charge_densities(rho, particle_positions, 1.0, dx, domain_length)
+
+    plt.figure()
+    plt.plot(x, n, label="Expected Charge Density")
+    plt.plot(x, rho, label="Calculated Charge Density")
+    plt.legend()
+    plt.show()
+
+
+def get_fields_test():
+    """
+    Simple sinusoidal density distribution to see if the expected E and potential fields are recovered
+    """
     num_x = 100
     x = np.linspace(0.0, 2 * np.pi, num_x)
     dx = x[1] - x[0]
@@ -118,3 +160,7 @@ if __name__ == '__main__':
     ax[2].set_title("Potential Field")
     ax[2].legend()
     plt.show()
+
+if __name__ == '__main__':
+    get_grid_densities_test()
+    get_fields_test()
--- pic/es1/particle_initialiser/particle_initialisers.py
+++ pic/es1/particle_initialiser/particle_initialisers.py
@@ -7,15 +7,18 @@ at the beginning of the implementation of ES1
 """
 
 from enum import Enum
+import numpy as np
+from matplotlib import pyplot as plt
 
 
 class ParticleDistributionType(Enum):
     UNIFORM = 1
+    PERTURBATION = 2
 
 
 class ParticleInitialisers(object):
     @staticmethod
-    def initialise_particles(particle_positions, particle_velocities, mean_velocity, distribution_type, domain_length):
+    def initialise_particles(particle_positions, particle_velocities, mean_velocity, distribution_type, domain_length, **kwargs):
         """
         Set up the particle distribution f(x, v, 0) at the beginning of the simulation.
 
@@ -26,7 +29,7 @@ class ParticleInitialisers(object):
         :param domain_length: length of the domain
         :return:
         """
-        assert particle_positions.size() == particle_velocities.size()
+        assert particle_positions.size == particle_velocities.size
 
         num_particles = particle_positions.size
         if distribution_type is ParticleDistributionType.UNIFORM:
@@ -34,5 +37,56 @@ class ParticleInitialisers(object):
             for i, pos in enumerate(particle_positions):
                 particle_positions[i] = i * particle_spacing
                 particle_velocities[i] = mean_velocity
+        elif distribution_type is ParticleDistributionType.PERTURBATION:
+            perturbation_amplitude = kwargs.get("perturbation_amplitude")
+            perturbation_velocity = kwargs.get("perturbation_velocity")
+            particle_spacing = domain_length / num_particles
+            # Construct uniform base field
+            for i, pos in enumerate(particle_positions):
+                particle_positions[i] = i * particle_spacing
+                particle_velocities[i] = mean_velocity
+            # Add perturbation
+            for i, pos in enumerate(particle_positions):
+                particle_velocities[i] += perturbation_velocity * np.cos(particle_positions[i])
+                particle_positions[i] += perturbation_amplitude * np.sin(particle_positions[i])
         else:
             raise ValueError("Specified option is not implemented!")
+
+
+def uniform_distribution_test():
+    """
+    Tests the implementation of a simple uniform distribution of particles
+    """
+    num_particles = 100
+    particle_positions = np.zeros(num_particles)
+    particle_velocities = np.zeros(num_particles)
+
+    ParticleInitialisers.initialise_particles(particle_positions, particle_velocities, 3.0, ParticleDistributionType.UNIFORM, 2 * np.pi)
+
+    fig, ax = plt.subplots(2)
+    ax[0].plot(particle_positions[1:] - particle_positions[:-1])
+    ax[1].plot(particle_velocities)
+    plt.show()
+
+
+def perturbed_distribution_test():
+    """
+    Tests the implementation of a simple perturbed distribution of particles
+    """
+    num_particles = 100
+    particle_positions = np.zeros(num_particles)
+    particle_velocities = np.zeros(num_particles)
+
+    ParticleInitialisers.initialise_particles(particle_positions, particle_velocities, 0.0,
+                                              ParticleDistributionType.PERTURBATION, 2 * np.pi,
+                                              perturbation_amplitude=0.5, perturbation_velocity=0.001)
+
+    fig, ax = plt.subplots(2)
+    ax[0].plot(particle_positions)
+    ax[1].plot(particle_velocities)
+    plt.show()
+
+
+if __name__ == '__main__':
+    uniform_distribution_test()
+    perturbed_distribution_test()
